d52b915f29e4d7671cb2459315c9da0b824dbcd1
Aon1840 <aonattapon1840.ap@gmail.com>
2018-04-01 22:22:40 +0700

    Add BlogAPI in api/models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlogAPI(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.title
